2c1b571458cd36f1d7ffb366fb52d0c2e802e72f
Yahya Abdul-Hakeem <42775710+ya7ya1994@users.noreply.github.com>
2022-05-26 16:26:36 +0100

    Add files via upload

---
+++ src/main/java/com/example/antra/springdemo/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.antra.springdemo.service;
+
+import com.example.antra.springdemo.exception.ResourceNotFoundException;
+import com.example.antra.springdemo.model.Employee;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public interface EmployeeService {
+    ResponseEntity<Employee> getEmployeeById(Long id) throws ResourceNotFoundException;
+    List<Employee> getAllEmployees();
+    Employee createEmployee(Employee employee);
+    ResponseEntity<Employee> updateEmployee(Long id, Employee employee) throws ResourceNotFoundException;
+    Map<String, Boolean> deleteEmployee(Long id) throws ResourceNotFoundException;
+}
